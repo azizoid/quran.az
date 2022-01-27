04662cfeb389abe08e22d05ca7f64a1064689e1b
azizoid <shaqhuseynov@gmail.com>
2022-01-27 18:55:27 +0100

    use iframe on facebook plugin

--- components/sidebar/facebook.page.tsx
+++ components/sidebar/facebook.page.tsx
@@ -2,7 +2,18 @@ import { Card } from "../../ui/Card/Card"
 
 export const FacebookPage = (): JSX.Element => (
   <Card title="BİZİ BƏYƏN" className={"px-0 py-0"}>
-    <div id="fb-root"></div>
+    <iframe
+      src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fquranaz%2F&tabs&width=340&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=10177953140"
+      width="340"
+      height="130"
+      style={{ border: "none", overflow: "hidden" }}
+      scrolling="no"
+      frameBorder="0"
+      allowFullScreen={true}
+      allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+    />
+
+    {/* <div id="fb-root"></div>
     <script
       async
       defer
@@ -28,7 +39,7 @@ export const FacebookPage = (): JSX.Element => (
       >
         <a href="https://www.facebook.com/quranaz/">quran.az</a>
       </blockquote>
-    </div>
+    </div> */}
   </Card>
 )
 
