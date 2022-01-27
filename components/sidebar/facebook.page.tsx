0644babc008adb23bf077771d027b6caa5adb29a
azizoid <shaqhuseynov@gmail.com>
2022-01-27 18:37:50 +0100

    update facebook plugin

--- components/sidebar/facebook.page.tsx
+++ components/sidebar/facebook.page.tsx
@@ -2,10 +2,20 @@ import { Card } from "../../ui/Card/Card"
 
 export const FacebookPage = (): JSX.Element => (
   <Card title="BİZİ BƏYƏN" className={"px-0 py-0"}>
+    <div id="fb-root"></div>
+    <script
+      async
+      defer
+      crossOrigin="anonymous"
+      src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0&appId=10177953140&autoLogAppEvents=1"
+      nonce="sv0YGg36"
+    ></script>
     <div
       className="fb-page object-fill w-full"
       data-href="https://www.facebook.com/quranaz/"
-      data-width="500"
+      data-tabs=""
+      data-width=""
+      data-height=""
       data-small-header="false"
       data-adapt-container-width="true"
       data-hide-cover="false"
@@ -16,7 +26,7 @@ export const FacebookPage = (): JSX.Element => (
         cite="https://www.facebook.com/quranaz/"
         className="fb-xfbml-parse-ignore"
       >
-        <a href="https://www.facebook.com/quranaz/">Quran.az</a>
+        <a href="https://www.facebook.com/quranaz/">quran.az</a>
       </blockquote>
     </div>
   </Card>
