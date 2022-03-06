027cbde3c0ce7223c701c518cfd8ea02189a0254
azizoid <shaqhuseynov@gmail.com>
2022-03-06 02:09:26 +0100

    add jest coverage hook

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,12 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
