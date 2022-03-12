bc4a9a4c70bd16fb1704a1243cf35d2e30f87f58
azizoid <shaqhuseynov@gmail.com>
2022-03-12 16:17:17 +0100

    test: form-store add query to the test

--- store/form-store.test.tsx
+++ store/form-store.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { prettyDOM, render } from '@testing-library/react'
 
 import { FormContextProvider } from './form-store'
 import { useRouter } from 'next/router';
@@ -11,6 +11,17 @@ jest.mock("next/router", () => ({
 
 test("FormContextProvider snapshot", ()=>{
   (useRouter as jest.Mock).mockImplementation(() => ({
+    push,
+  }));
+
+  const { container } = render(<FormContextProvider/>)
+
+  expect(container).toMatchSnapshot()
+})
+
+test("FormContextProvider snapshot", ()=>{
+  (useRouter as jest.Mock).mockImplementation(() => ({
+    query:{soorah:23, ayah:23, search:'Musa', t:1},
     push,
   }));
 
