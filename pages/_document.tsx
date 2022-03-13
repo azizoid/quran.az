a0a8b51aac73f54e3fa832c0541be3de2d74ba28
azizoid <shaqhuseynov@gmail.com>
2022-03-13 10:16:02 +0100

    add az for html lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,12 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="bs">
+      <Html lang="az">
         <Head>
           <link rel="shortcut icon" href="/static/favicon.ico" />
           <meta name="author" content="Aziz" />
           <meta httpEquiv="content-language" content="az,ru" />
+          <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylescheet" />
         </Head>
         <body className="bg-gray-50">
           <Main />
