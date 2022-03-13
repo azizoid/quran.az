d3631ade640ddeb19abaf4c4d734ec4e853f8a2b
azizoid <shaqhuseynov@gmail.com>
2022-03-13 13:07:47 +0100

    fit facebook iframe to the content

--- components/sidebar/facebook.page.tsx
+++ components/sidebar/facebook.page.tsx
@@ -1,45 +1,19 @@
 import { Card } from '../../ui/Card/Card'
 
 export const FacebookPage = (): JSX.Element => (
-  <Card title="BİZİ BƏYƏN" className={'px-0 py-0'}>
-    <iframe
-      src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fquranaz%2F&tabs&width=340&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=10177953140"
-      width="340"
-      height="130"
-      style={{ border: 'none', overflow: 'hidden' }}
-      scrolling="no"
-      frameBorder="0"
-      allowFullScreen={true}
-      allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-    />
-
-    {/* <div id="fb-root"></div>
-    <script
-      async
-      defer
-      crossOrigin="anonymous"
-      src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0&appId=10177953140&autoLogAppEvents=1"
-      nonce="sv0YGg36"
-    ></script>
-    <div
-      className="fb-page object-fill w-full"
-      data-href="https://www.facebook.com/quranaz/"
-      data-tabs=""
-      data-width=""
-      data-height=""
-      data-small-header="false"
-      data-adapt-container-width="true"
-      data-hide-cover="false"
-      data-show-facepile="true"
-      data-lazy="true"
-    >
-      <blockquote
-        cite="https://www.facebook.com/quranaz/"
-        className="fb-xfbml-parse-ignore"
-      >
-        <a href="https://www.facebook.com/quranaz/">quran.az</a>
-      </blockquote>
-    </div> */}
+  <Card title="BİZİ BƏYƏN" size="fit" titleClassName="px-7">
+    <div className="flex justify-center">
+      <iframe
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fquranaz%2F&tabs&width=340&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=10177953140"
+        width="340"
+        height="130"
+        style={{ border: 'none', overflow: 'hidden' }}
+        scrolling="no"
+        frameBorder="0"
+        allowFullScreen={true}
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+      />
+    </div>
   </Card>
 )
 
