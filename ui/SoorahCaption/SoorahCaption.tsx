550626a3872133af1b8642ea4e7d5e1a8de001a3
azizoid <shaqhuseynov@gmail.com>
2022-04-24 03:18:43 +0200

    destructure soorah data in SoorahCaption

--- ui/SoorahCaption/SoorahCaption.tsx
+++ ui/SoorahCaption/SoorahCaption.tsx
@@ -6,25 +6,23 @@ import { CityAndSize } from '@/ui'
 export type SoorahCaptionProps = {
   soorah: number
   translator: number
-  ayahCount?: number // TODO
 }
 
-export const SoorahCaption = ({ soorah, translator }: SoorahCaptionProps): JSX.Element => (
-  <li
-    className={`ayah-list-item text-center text-lg md:text-2xl font-thin ${styles.header} flex align-middle justify-center whitespace-nowrap`}
-  >
-    <Link href={`/${soorah}?t=${translator}`}>
-      <a className="text-gray-400 hover:text-black decoration-1">
-        {`${soorah}. ${SOORAH_LIST[soorah]['fullTitle']}`}
-        <span className="flex flex-row justify-center gap-1 text-xs">
-          <CityAndSize
-            city={SOORAH_LIST[soorah]['city']}
-            ayahCount={SOORAH_LIST[soorah]['ayahCount']}
-            devider={true}
-            size="sm"
-          />
-        </span>
-      </a>
-    </Link>
-  </li>
-)
+export const SoorahCaption = ({ soorah, translator }: SoorahCaptionProps): JSX.Element => {
+  const { fullTitle, city, ayahCount } = SOORAH_LIST[soorah]
+
+  return (
+    <li
+      className={`ayah-list-item text-center text-lg md:text-2xl font-thin ${styles.header} flex align-middle justify-center whitespace-nowrap`}
+    >
+      <Link href={`/${soorah}?t=${translator}`}>
+        <a className="text-gray-400 hover:text-black decoration-1">
+          {`${soorah}. ${fullTitle}`}
+          <span className="flex flex-row justify-center gap-1 text-xs">
+            <CityAndSize city={city} ayahCount={ayahCount} devider={true} size="sm" />
+          </span>
+        </a>
+      </Link>
+    </li>
+  )
+}
