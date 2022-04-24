69063839c6405f39cd7540d775008e6fcb9c7c6e
azizoid <shaqhuseynov@gmail.com>
2022-04-24 02:17:51 +0200

    feat: show which city soorah has revealed and how many ayahs it has

--- components/SoorahLIstItem/SoorahListItem.tsx
+++ components/SoorahLIstItem/SoorahListItem.tsx
@@ -1,5 +1,6 @@
 import { SoorahListProps } from '@/assets/soorah-list-object'
 import Link from 'next/link'
+import { FaKaaba, FaMosque } from 'react-icons/fa'
 
 export type SoorahListItemProps = {
   soorah: SoorahListProps
@@ -7,19 +8,35 @@ export type SoorahListItemProps = {
 
 export const SoorahListItem = ({ soorah }: SoorahListItemProps): JSX.Element => (
   <Link href={`/${soorah.id}`}>
-    <a className="flex w-full my-4 mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
-      <div className="flex items-center justify-center w-12 bg-emerald-50 hover:bg-emerald-500">
-        <span className="badge badge-white">{soorah.id}</span>
+    <a className="group flex w-full my-4 mx-auto overflow-hidden bg-white rounded-lg shadow-md">
+      <div className="flex items-center justify-center w-14 bg-emerald-50 group-hover:bg-emerald-500 group-hover:text-white">
+        {soorah.id}
       </div>
 
-      <div className="px-4 py-2 -mx-3">
+      <div className="px-4 py-2 -mx-3 flex flex-row w-full items-center justify-between">
         <div className="mx-3">
-          <span className="font-semibold text-emerald-500 dark:text-emerald-400">
-            {soorah.title}
-          </span>
-          <p className="text-sm text-gray-600 dark:text-gray-200">{soorah.fullTitle}</p>
-          <span className="text-sm text-gray-600 dark:text-gray-200">{soorah.ayahCount} ayə</span>
+          <span className="font-semibold text-emerald-500">{soorah.title}</span>
+
+          <p className="text-sm text-gray-600 w-full flex flex-row justify-start gap-3">
+            <span className="flex gap-1 align-middle whitespace-nowrap">
+              {soorah.city === 'Mecca' ? (
+                <>
+                  <FaKaaba size={18} className="text-gray-300 group-hover:text-gray-600" />
+                  Məkkə surəsi
+                </>
+              ) : (
+                <>
+                  <FaMosque size={18} className="text-gray-300 group-hover:text-gray-600" />
+                  Mədinə surəsi
+                </>
+              )}
+            </span>
+            /<span>{soorah.ayahCount} ayə</span>
+          </p>
         </div>
+      </div>
+      <div className="flex items-center justify-center px-5 text-right whitespace-nowrap text-lg text-gray-600 bg-emerald-50 group-hover:bg-emerald-500 group-hover:text-white">
+        {soorah.arabic}
       </div>
     </a>
   </Link>
