d11991c4f82b9fe547ca31dd102e5b030efd034a
azizoid <shaqhuseynov@gmail.com>
2022-05-27 11:44:24 +0200

    fix conditional chaining to the default form elements

--- utility/getView/getView.ts
+++ utility/getView/getView.ts
@@ -12,20 +12,20 @@ export const initialStateProps: FormProps = {
 export const getView = (form: Partial<FormProps>): FormProps => {
   const result = {} as FormProps
 
-  if (form.s && form.s > 0 && form.s < 115) {
+  if (form?.s && form.s > 0 && form.s < 115) {
     result.s = form.s
     result.view = 'soorah'
 
-    if (form.a && form.a > 0 && form.a < 287) {
+    if (form?.a && form.a > 0 && form.a < 287) {
       result.a = form.a
       result.view = 'ayah'
     }
-  } else if (form.q && form.q.length > 2) {
+  } else if (form?.q && form.q.length > 2) {
     result.q = form.q
     result.view = 'search'
   }
 
-  if (form.t < TRANSLATOR_LIST.length) {
+  if (form?.t < TRANSLATOR_LIST.length) {
     result.t = form.t
   }
 
