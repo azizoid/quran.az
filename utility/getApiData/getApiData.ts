e560e6f1cb0f6d4fbf3899885af80f725509cc8a
azizoid <shaqhuseynov@gmail.com>
2022-12-30 00:34:11 +0400

    bug: get rid of full url in the fetch request

--- utility/getApiData/getApiData.ts
+++ utility/getApiData/getApiData.ts
@@ -1,11 +1,8 @@
-export const getApiData = async (url: string) => {
-  const fullUrl = `${process.env.NEXT_PUBLIC_URL}${url}`
-
-  return await fetch(fullUrl).then((response) => {
+export const getApiData = async (url: string) =>
+  await fetch(url).then((response) => {
     if (!response.ok) {
       // throw Error(response.statusText);
       // silently ignore
     }
     return response.json()
   }) //  workaround the catch
-}
