513554d2d2f242546b15fdce4734d2b3963a4724
azizoid <shaqhuseynov@gmail.com>
2023-03-21 14:13:50 +0100

    feature: use nextjs's `notFound:true` for ayah page view

--- pages/[soorah]/[ayah].tsx
+++ pages/[soorah]/[ayah].tsx
@@ -7,45 +7,35 @@ import { ColoredText, Bismillah, SoorahCaption, soorahAyahTitle } from '@/ui'
 
 import { getApiData } from '@/utility'
 
-import { PageStates } from '@/lib/types'
-
 import { PaginateAyah } from '@/components'
 
-export const Ayah = ({ out, error }) => {
-  if (error === PageStates.NOT_FOUND) {
-    return <div className="col-sm-12 alert alert-danger">Ayə tapılmamışdır</div>
-  }
+export const Ayah = ({ soorah, ayah, content, translator, arabic, transliteration, prev, next }) => (
+  <>
+    <Head>
+      <title>{`${soorahAyahTitle(soorah, ayah)}, | Öz Kitabını oxu | quran.az`}</title>
+      <meta name="description" content={content} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
 
-  const { soorah, ayah, content, translator, arabic, transliteration, prev, next } = out
-
-  return (
-    <>
-      <Head>
-        <title>{`${soorahAyahTitle(soorah, ayah)}, | Öz Kitabını oxu | quran.az`}</title>
-        <meta name="description" content={content} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-
-      <ul className="list-none divide-y divide-gray-100 bg-white text-gray-700 mb-4">
-        <SoorahCaption soorah={soorah} translator={translator} />
-        {soorah !== 1 && ayah !== 1 && <Bismillah />}
-        <li className="ayah-list-item flex flex-col">
-          <span className="text-gray-400">{soorahAyahTitle(soorah, ayah)}</span>
-          {content}
-        </li>
-        <li className="ayah-list-item ">
-          <ColoredText key="transliteration" content={transliteration} />
-        </li>
-        <li className="ayah-list-item text-3xl font-Nunito text-right" dir="rtl">
-          {arabic}
-        </li>
-        <li>
-          <PaginateAyah {...{ soorah, ayah, prev, next, translator }} />
-        </li>
-      </ul>
-    </>
-  )
-}
+    <ul className="list-none divide-y divide-gray-100 bg-white text-gray-700 mb-4">
+      <SoorahCaption soorah={soorah} translator={translator} />
+      {soorah !== 1 && ayah !== 1 && <Bismillah />}
+      <li className="ayah-list-item flex flex-col">
+        <span className="text-gray-400">{soorahAyahTitle(soorah, ayah)}</span>
+        {content}
+      </li>
+      <li className="ayah-list-item ">
+        <ColoredText key="transliteration" content={transliteration} />
+      </li>
+      <li className="ayah-list-item text-3xl font-Nunito text-right" dir="rtl">
+        {arabic}
+      </li>
+      <li>
+        <PaginateAyah {...{ soorah, ayah, prev, next, translator }} />
+      </li>
+    </ul>
+  </>
+)
 
 Ayah.getLayout = (page: ReactElement) => {
   return <MainLayout>{page}</MainLayout>
@@ -60,23 +50,19 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
     `${process.env.NEXT_PUBLIC_URL}/api/${soorah}/${ayah}?t=${translator}`
   )
 
-  if (res?.success) {
+  if (!res?.success) {
+
     return {
-      props: {
-        error: '',
-        out: res.out,
-        data: res.data,
-      },
+      notFound: true
     }
   }
 
   return {
     props: {
-      error: PageStates.NOT_FOUND,
-      out: {},
-      data: { s: 0, a: '', translator },
+      ...res.out
     },
   }
+
 }
 
 // eslint-disable-next-line import/no-default-export
--- pages/api/[soorah]/[ayah].ts
+++ pages/api/[soorah]/[ayah].ts
@@ -32,74 +32,73 @@ const handler = async (
   const translator = Number(query.t?.toString() || process.env.DEFAULT_TRANSLATOR)
   const data = getView({ s: soorah, a: ayah, t: translator })
 
-  if (data.view === 'empty') {
-    return res.status(400).json({ success: false })
-  }
 
-  switch (method) {
-    case 'GET':
-      try {
-        const out = await withMongo(async (db: Db) => {
-          const contentCollection = db.collection('quranaz');
 
-          const content = await contentCollection.aggregate<AyahResponseType>([
-            {
-              $match: {
-                soorah: Number(data.s),
-                ayah: Number(data.a),
-                translator: data.t
-              }
-            },
-            {
-              $lookup: {
-                from: 'metadata',
-                localField: 'metadata_id',
-                foreignField: '_id',
-                as: 'metadata'
-              }
-            },
-            {
-              $unwind: '$metadata'
-            },
-            {
-              $project: {
-                _id: 1,
-                id: 1,
-                soorah: 1,
-                ayah: 1,
-                translator: 1,
-                content: 1,
-                content_latinized: 1,
-                arabic: '$metadata.content',
-                transliteration: '$metadata.transliteration',
-                juz: '$metadata.juz'
-              }
-            }
-          ]).next();
+  try {
+    if (method !== 'GET' || data.view === 'empty') {
+      throw new Error('Ayah not found')
+    }
 
-          const prevAyah = Number(data.a) - 1
-          const nextAyah = Number(data.a) + 1
+    const out = await withMongo(async (db: Db) => {
+      const contentCollection = db.collection('quranaz');
 
-          const prevAndNext = await contentCollection.find<{ ayah: number }>({
-            soorah: data.s,
-            ayah: { $in: [prevAyah, nextAyah] },
-            translator: data.t,
-          }, { projection: { ayah: 1, _id: 0 } }).toArray()
+      const content = await contentCollection.aggregate<AyahResponseType>([
+        {
+          $match: {
+            soorah: Number(data.s),
+            ayah: Number(data.a),
+            translator: data.t
+          }
+        },
+        {
+          $lookup: {
+            from: 'metadata',
+            localField: 'metadata_id',
+            foreignField: '_id',
+            as: 'metadata'
+          }
+        },
+        {
+          $unwind: '$metadata'
+        },
+        {
+          $project: {
+            _id: 1,
+            id: 1,
+            soorah: 1,
+            ayah: 1,
+            translator: 1,
+            content: 1,
+            content_latinized: 1,
+            arabic: '$metadata.content',
+            transliteration: '$metadata.transliteration',
+            juz: '$metadata.juz'
+          }
+        }
+      ]).next();
 
-          const prev = prevAndNext.find((item) => item.ayah === prevAyah)?.ayah || null
-          const next = prevAndNext.find((item) => item.ayah === nextAyah)?.ayah || null
-
-          return { ...content, prev, next }
-        })
-
-        return res.json({ out, data, success: true })
-      } catch (error) {
-        res.status(400).json({ success: false })
+      if (!content) {
+        throw new Error('Ayah not found')
       }
-      break
-    default:
-      res.status(400).json({ success: false })
-      break
+
+      const prevAyah = Number(data.a) - 1
+      const nextAyah = Number(data.a) + 1
+
+      const prevAndNext = await contentCollection.find<{ ayah: number }>({
+        soorah: data.s,
+        ayah: { $in: [prevAyah, nextAyah] },
+        translator: data.t,
+      }, { projection: { ayah: 1, _id: 0 } }).toArray()
+
+      const prev = prevAndNext.find((item) => item.ayah === prevAyah)?.ayah || null
+      const next = prevAndNext.find((item) => item.ayah === nextAyah)?.ayah || null
+
+      return { ...content, prev, next }
+    })
+
+    return res.json({ out, data, success: true })
+  } catch (error) {
+    res.status(400).json({ success: false, error: String(error) })
   }
 }
 
