fe83153cf0a7f44a91946f604660da8214bbd425
azizoid <shaqhuseynov@gmail.com>
2023-03-21 22:27:01 +0100

    fix: add legacy redirect rules

--- next.config.js
+++ next.config.js
@@ -10,4 +10,26 @@ module.exports = {
   experimental: {
     largePageDataBytes: 128 * 100000,
   },
+  redirects: async () => [
+    {
+      source: '/aze/:soorah',
+      destination: '/:soorah',
+      permanent: true,
+    },
+    {
+      source: '/aze/:soorah/:ayah',
+      destination: '/:soorah/:ayah',
+      permanent: true,
+    },
+    {
+      source: '/rus/:soorah',
+      destination: '/:soorah?t=3',
+      permanent: true,
+    },
+    {
+      source: '/rus/:soorah/:ayah',
+      destination: '/:soorah/:ayah?t=3',
+      permanent: true,
+    }
+  ],
 }
