109fe1738e57fc9aa2f0f38d94a7da234921d4c1
azizoid <shaqhuseynov@gmail.com>
2023-05-21 22:59:34 +0200

    refactor: read the soorah information from the array

--- src/utility/getView/getView.ts
+++ src/utility/getView/getView.ts
@@ -1,22 +1,27 @@
+import { soorahList } from '@/assets/soorah-list-object'
 import { TRANSLATOR_LIST } from 'src/assets/translatorList'
 import { FormProps } from 'src/lib/types'
 
-export const initialStateProps: FormProps = {
+const DEFAULT_TRANSLATOR = Number(process.env.NEXT_PUBLIC_DEFAULT_TRANSLATOR)
+
+const initialStateProps: FormProps = {
   s: 0,
   a: '',
   q: '',
   view: 'empty',
-  t: Number(process.env.NEXT_PUBLIC_DEFAULT_TRANSLATOR),
+  t: DEFAULT_TRANSLATOR,
 }
 
 export const getView = (form: Partial<FormProps>): FormProps => {
-  const result = {} as FormProps
+  const result: Partial<FormProps> = {}
 
-  if (form?.s && form.s > 0 && form.s < 115) {
-    result.s = form.s
+  const soorah = soorahList.find(({ id }) => id === form.s)
+
+  if (form?.s && soorah) {
+    result.s = soorah.id
     result.view = 'soorah'
 
-    if (form?.a && form.a > 0 && form.a < 287) {
+    if (form?.a && form.a > 0 && form.a <= soorah.ayahCount) {
       result.a = form.a
       result.view = 'ayah'
     }
