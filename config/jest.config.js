9345852e88d99147176a74e9b05dd2a17c8eb88f
azizoid <shaqhuseynov@gmail.com>
2023-05-23 01:35:05 +0200

    chore: cleanup the root folder

--- config/jest.config.js
+++ config/jest.config.js
@@ -13,7 +13,8 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  rootDir: '..',
+  setupFilesAfterEnv: ['<rootDir>/config/jest-setup.ts'],
   collectCoverage: true,
   coverageThreshold: {
     global: {
