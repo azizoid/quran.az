d83632c5aee188cda59bc9dbb450314ec24ab75e
azizoid <shaqhuseynov@gmail.com>
2023-06-11 02:16:46 +0200

    add a POST api endpoint for search

---
+++ src/app/api/v2/search/route.ts
@@ -0,0 +1,95 @@
+import * as Sentry from '@sentry/node'
+import { NextResponse } from 'next/server'
+
+import { Db } from 'mongodb'
+
+import { DisplayData, FormProps, DataPropsLatinized, ResponseData } from '@/lib/types'
+import { getView } from '@/utility/getView/getView'
+import { withMongo } from '@/utility/mongodb'
+import { initialPaginate } from '@/utility/paginate/paginate'
+
+export type ReponseProps = ResponseData & {
+  out: DisplayData[]
+  data?: FormProps
+  paginate: {
+    total: number
+    perPage: number
+    currentPage: number
+  }
+}
+
+const REGEX_SANITIZE = /[-\/\\^$*+?.()|[\]{}]/g
+const REGEX_DIACRITICS = /[\u0300-\u036f]/g
+
+export const POST = async (request: Request) => {
+  try {
+    const content = await request.formData()
+
+    const search = content.get('search')?.toString()
+      .replace(REGEX_SANITIZE, '\\$&')
+      .normalize('NFD')
+      .replace(REGEX_DIACRITICS, '')
+      .trim()
+
+    if (!search) {
+      return NextResponse.json({ error: 'Search query is required' }, { status: 400 })
+    }
+
+    const currentPage = Number(content.get('page')?.toString()) || 1
+    const translator = Number(content.get('t')?.toString() || process.env.DEFAULT_TRANSLATOR)
+
+    const limit = initialPaginate.perPage
+    const skip = (currentPage - 1) * limit
+
+    const data = getView({
+      q: search,
+      t: translator
+    })
+
+    let ayahsCount = 0
+
+    const ayahs = await withMongo(async (db: Db) => {
+      const collection = db.collection<DataPropsLatinized>('quranaz')
+
+      if (!data.q) return [] // very rare edge case
+
+      const searchQuery = {
+        content_latinized: { '$regex': data.q, '$options': 'i' },
+        translator: data.t,
+      }
+
+      ayahsCount = await collection.countDocuments(searchQuery)
+
+      const result = await collection
+        .find(searchQuery)
+        .sort({ soorah: 1, ayah: 1 }) // Sort by soorah in ascending order, then by ayah in ascending order
+        .skip(skip)
+        .limit(limit)
+        .toArray()
+
+      return result
+    })
+
+    if (ayahs.length === 0) {
+      return NextResponse.json({ error: 'No results found for the given search query.' }, { status: 200 })
+    }
+
+    return NextResponse.json({
+      out: ayahs,
+      data,
+      paginate: {
+        ...initialPaginate,
+        total: ayahsCount,
+        currentPage,
+      },
+      success: true,
+    }, {
+      status: 200, headers: {
+        'Cache-Control': 'no-store'
+      },
+    })
+  } catch (error) {
+    Sentry.captureException(error)
+    return NextResponse.json({ error: (error as Error).message }, { status: 500 })
+  }
+}
