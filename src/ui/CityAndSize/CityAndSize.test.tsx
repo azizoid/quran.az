2cc92096574e382a02da04fca5c29c44328e58d2
azizoid <shaqhuseynov@gmail.com>
2023-07-09 23:56:50 +0200

    test: test devider in the CityAndSize component

--- src/ui/CityAndSize/CityAndSize.test.tsx
+++ src/ui/CityAndSize/CityAndSize.test.tsx
@@ -1,9 +1,17 @@
-import { render } from '@testing-library/react'
+import { render, screen, prettyDOM } from '@testing-library/react'
 
 import { CityAndSize } from './CityAndSize'
 
 test('Render CityAndSize', () => {
-  const { container } = render(<CityAndSize city="Fatiha" ayahCount={7} devider={true} />)
+  const { container } = render(<CityAndSize city="Fatiha" ayahCount={7} devider={false} />)
 
   expect(container).toMatchSnapshot()
+})
+
+test('Render CityAndSize without devider', () => {
+  render(<CityAndSize city="Fatiha" ayahCount={7} devider={true} />)
+
+  const devider = screen.queryByText('/')
+
+  expect(devider).toHaveTextContent('/')
 })
