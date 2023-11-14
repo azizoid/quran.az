c6a22b268f6f563ca12f2a268815a1907883c6c0
azizoid <shaqhuseynov@gmail.com>
2023-11-14 01:27:13 +0100

    fix: wrap Form component into Suspense to get rid of the `Entire page / deopted` error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Suspense } from 'react'
+
 import { Empty } from '@/components/Empty/Empty'
 import { Form } from '@/components/Form/Form'
+import { LoaderProgress } from '@/ui'
 
 const Home = () => (
   <>
-    <Form />
+    <Suspense fallback={<LoaderProgress />}>
+      <Form />
+    </Suspense>
 
     <Empty />
   </>
