f52252d73a3e33172e2f293fe78b5da927a0badb
azizoid <shaqhuseynov@gmail.com>
2024-01-14 01:18:47 +0100

    remove footer test

--- src/ui/Footer/Footer.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-
-import { Footer } from './Footer'
-
-test('Render Footer', () => {
-  const { container } = render(<Footer />)
-
-  expect(container).toMatchSnapshot()
-})
