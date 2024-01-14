ea860c81ecc86be405c92d94fdf1470244168701
azizoid <shaqhuseynov@gmail.com>
2024-01-14 17:08:07 +0100

    update types

--- src/utility/getView/getView.ts
+++ src/utility/getView/getView.ts
@@ -3,35 +3,39 @@ import { TRANSLATOR_LIST } from '@/assets/translatorList'
 
 const DEFAULT_TRANSLATOR = Number(process.env.NEXT_PUBLIC_DEFAULT_TRANSLATOR)
 
-export type FormProps =
-  | {
-      view: 'empty'
-      s: null
-      a: null
-      q: null
-      t: number
-    }
-  | {
-      view: 'soorah'
-      s: number
-      a: null
-      q: null
-      t: number
-    }
-  | {
-      view: 'ayah'
-      s: number
-      a: number
-      q: null
-      t: number
-    }
-  | {
-      view: 'search'
-      s: null
-      a: null
-      q: string
-      t: number
-    }
+type EmptyState = {
+  view: 'empty'
+  s: null
+  a: null
+  q: null
+  t: number
+}
+
+type SoorahState = {
+  view: 'soorah'
+  s: number
+  a: null
+  q: null
+  t: number
+}
+
+type AyahState = {
+  view: 'ayah'
+  s: number
+  a: number
+  q: null
+  t: number
+}
+
+type SearchState = {
+  view: 'search'
+  s: null
+  a: null
+  q: string
+  t: number
+}
+
+export type FormProps = EmptyState | SoorahState | AyahState | SearchState
 
 const initialStateProps: FormProps = {
   s: null,
