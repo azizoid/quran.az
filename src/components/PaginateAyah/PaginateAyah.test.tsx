269158b583b93777ea2a7c37d5eb5b08d1c75454
azizoid <shaqhuseynov@gmail.com>
2024-01-22 04:45:03 +0100

    add extra props to the PaginasteAyah tests

--- src/components/PaginateAyah/PaginateAyah.test.tsx
+++ src/components/PaginateAyah/PaginateAyah.test.tsx
@@ -25,8 +25,21 @@ describe('PaginateAyah', () => {
     expect(nextLink).toHaveAttribute('href', '/2/6?t=1')
   })
 
-  test('renders the first ayah when prev Soorah is available', () => {
+  test('renders the first ayah when prev Soorah is available with ayahOnly', () => {
     render(<PaginateAyah {...mockProps} soorah={2} ayah={1} />)
+
+    const ayahText = screen.getByText('1')
+    const prevSoorahLink = screen.queryByRole('link', { name: '1. əl-Fatihə surəsi ←' })
+    const nextLink = screen.getByRole('link', { name: '2' })
+
+    expect(ayahText).toHaveClass('pagination-disabled')
+    expect(prevSoorahLink).not.toBeInTheDocument()
+
+    expect(nextLink).toBeInTheDocument()
+  })
+
+  test('renders the first ayah when prev Soorah is available', () => {
+    render(<PaginateAyah {...mockProps} soorah={2} ayah={1} ayahOnly={false} />)
 
     const ayahText = screen.getByText('1')
     const prevSoorahLink = screen.getByRole('link', { name: '1. əl-Fatihə surəsi ←' })
@@ -38,8 +51,21 @@ describe('PaginateAyah', () => {
     expect(nextLink).toBeInTheDocument()
   })
 
-  test('renders the last ayah when nextSoorah is available', () => {
+  test('renders the last ayah when nextSoorah is available with ayahOnly', () => {
     render(<PaginateAyah {...mockProps} soorah={1} ayah={7} />)
+
+    const ayahText = screen.getByText('7')
+    const prevSoorahLink = screen.getByRole('link', { name: '6' })
+    const nextLink = screen.queryByRole('link', { name: '2. əl-Bəqərə surəsi →' })
+
+    expect(ayahText).toHaveClass('pagination-disabled')
+    expect(prevSoorahLink).toBeInTheDocument()
+
+    expect(nextLink).not.toBeInTheDocument()
+  })
+
+  test('renders the last ayah when nextSoorah is available', () => {
+    render(<PaginateAyah {...mockProps} soorah={1} ayah={7} ayahOnly={false} />)
 
     const ayahText = screen.getByText('7')
     const prevSoorahLink = screen.getByRole('link', { name: '6' })
