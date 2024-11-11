2a11eafcf0ed7146cbf69933967bd3ff3535373f
azizoid <shaqhuseynov@gmail.com>
2024-11-11 01:49:10 +0100

    upgrade packages to next15, react19

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-/** @type {import("next").NextConfig} */
-module.exports = {
-  swcMinify: true,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   compress: true,
   poweredByHeader: false,
   generateEtags: false,
@@ -86,4 +86,6 @@ module.exports = {
       permanent: true,
     },
   ],
-}
+};
+
+export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ['./src/**/*.{jsx,tsx}'],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
