49eef06ff220d4d43f7c70a99dfa4f477c6b21de
azizoid <shaqhuseynov@gmail.com>
2024-11-12 04:11:11 +0100

    cleanup fat from tailwind

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -137,7 +137,7 @@ export const Form = (): JSX.Element => {
           onChange={onHandleChange}
         />
 
-        <button className="btn btn-success col-span-5" type="submit">
+        <button className="col-span-5 bg-green-200 py-2 px-3 rounded-md shadow text-green-900 hover:bg-green-300" type="submit">
           Axtar
         </button>
       </div>
