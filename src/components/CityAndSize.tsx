e0ed15fa0da52123730b17384169460a973e69df
azizoid <shaqhuseynov@gmail.com>
2024-11-15 06:42:51 +0100

    style: DRY tanzil city component

--- src/components/CityAndSize.tsx
+++ src/components/CityAndSize.tsx
@@ -1,39 +1,12 @@
-import { FaKaaba, FaMosque } from 'react-icons/fa'
+import { RevealPlace, RevealPlaceProps } from './RevealPlace'
 
-import { IconType } from 'react-icons'
-
-const iconSizes = {
-  sm: 16,
-  md: 18,
-}
-
-const cityList = {
-  Mecca: {
-    Icon: FaKaaba,
-    title: 'Məkkə surəsi',
-  },
-  Medina: {
-    Icon: FaMosque,
-    title: 'Mədinə surəsi',
-  },
-}
-
-const CityFormatted = (Icon: IconType, title: string, size: number) => (
-  <span className="flex gap-1 align-middle whitespace-nowrap">
-    <Icon size={size} className="text-gray-300 group-hover:text-gray-600" />
-    {title}
-  </span>
-)
-
-type CityAndSizeProps = {
-  city: keyof typeof cityList
+interface CityAndSizeProps extends RevealPlaceProps {
   ayahCount: number
-  size?: keyof typeof iconSizes
 }
 
 export const CityAndSize = ({ city, ayahCount, size = 'sm' }: CityAndSizeProps) => (
   <>
-    {CityFormatted(cityList[city].Icon, cityList[city].title, iconSizes[size])}
+    <RevealPlace city={city} size={size} />
     <span>{'/'}</span>
     <span>{ayahCount} ayə</span>
   </>
---
+++ src/components/RevealPlace.tsx
@@ -0,0 +1,43 @@
+import { FaKaaba, FaMosque } from 'react-icons/fa'
+
+import { IconType } from 'react-icons'
+
+import { TanzilCityType } from '@/helpers/types'
+
+const iconSizes = {
+  sm: 16,
+  md: 18,
+}
+
+const cityList: Record<
+  TanzilCityType,
+  {
+    Icon: IconType
+    title: string
+  }
+> = {
+  Mecca: {
+    Icon: FaKaaba,
+    title: 'Məkkə surəsi',
+  },
+  Medina: {
+    Icon: FaMosque,
+    title: 'Mədinə surəsi',
+  },
+}
+
+export type RevealPlaceProps = {
+  city: TanzilCityType
+  size?: keyof typeof iconSizes
+}
+
+export const RevealPlace = ({ city, size = 'md' }: RevealPlaceProps) => {
+  const { Icon, title } = cityList[city]
+
+  return (
+    <span className="flex gap-1 align-middle whitespace-nowrap">
+      <Icon size={iconSizes[size]} className="text-gray-300 group-hover:text-gray-600" />
+      {title}
+    </span>
+  )
+}
