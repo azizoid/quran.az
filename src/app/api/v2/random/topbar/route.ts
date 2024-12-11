2c9426b9e664a7507eb942363e54bb17c657aeea
azizoid <shaqhuseynov@gmail.com>
2024-12-11 17:32:48 +0100

    feature: show random 3 soorahs on the main page, cache every hour

--- src/app/api/v2/random/topbar/route.ts
+++ src/app/api/v2/random/topbar/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from 'next/server'
 import { soorahList } from '@/assets/soorah-list-object'
 import { selectRandomItems } from '@/utility/selectRandomItems'
 
-export const revalidate = 10
+export const revalidate = 3600
 
 export async function GET() {
-  const randomItems = selectRandomItems(soorahList, 5)
+  const randomItems = selectRandomItems(soorahList, 3)
   return NextResponse.json(randomItems)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,36 @@
+import { SoorahListProps } from '@/assets/soorah-list-object'
 import { Link } from '@/components/Link'
 import { SoorahList } from '@/components/SoorahList'
+import { fetcher } from '@/utility/fetcher'
 
-const topLinks = [
-  ['/1', 'Fatihə surəsi'],
-  ['/36', 'Ya-sin surəsi'],
-  ['/55', 'Ər-Rəhman surəsi'],
-  ['/67', 'Əl-Mülk surəsi'],
-  ['/2/255', 'Ayətul-Kürsi'],
-]
+export const revalidate = 3600
 
-const Home = () => (
-  <div className="space-y-4 px-2 py-2">
-    <ol className="flex w-full justify-center space-x-5 text-sm text-gray-500 md:text-base">
-      {topLinks.map(([url, urlText]) => (
-        <li key={url}>
-          <Link href={url}>{urlText}</Link>
-        </li>
-      ))}
-    </ol>
+const Home = async () => {
+  const topLinks = fetcher([
+    `${process.env.NEXT_PUBLIC_BASE_API_URL}/random/topbar`,
+    'topbar-links',
+  ]) as unknown as SoorahListProps[]
 
-    <SoorahList />
-  </div>
-)
+  return (
+    <div className="space-y-4 px-2 py-2">
+      <ol className="flex w-full justify-center space-x-5 text-sm text-gray-500 md:text-base">
+        {topLinks
+          .sort((a, b) => a.id - b.id)
+          .map(({ id, fullTitle }) => (
+            <li key={id} className="group relative flex items-center justify-center">
+              <Link
+                href={`/${id}`}
+                className="inline-block rounded-lg px-4 py-2 text-center transition-all duration-300 hover:bg-gray-200 hover:text-gray-900 focus:ring-2 focus:ring-gray-400 active:bg-gray-300"
+              >
+                {`${id}. ${fullTitle}`}
+              </Link>
+            </li>
+          ))}
+      </ol>
+
+      <SoorahList />
+    </div>
+  )
+}
 
 export default Home
