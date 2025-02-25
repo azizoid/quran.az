a431a55c7d1c2f0ce61fb15b5b83d1c767aee43d
azizoid <shaqhuseynov@gmail.com>
2025-02-25 15:38:53 +0100

    fix: update ESLint configuration and dependencies for improved linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { FlatCompat } from '@eslint/eslintrc'
 
+import propperNextJsPlugin from 'eslint-plugin-propper-nextjs'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
@@ -12,42 +13,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  propperNextJsPlugin.configs.recommended,
   {
     ignores: ['node_modules', '.next'],
   },
   {
     rules: {
-      'no-console': 'error',
       'semi': ['error', 'never'],
-      'quotes': ['error', 'single'],
-      'no-mixed-spaces-and-tabs': 'error',
-      'no-trailing-spaces': 'error',
-      'no-multi-spaces': 'error',
-      'arrow-spacing': 'error',
-      'prefer-arrow-callback': 'error',
-      'brace-style': [ 'error', '1tbs', { 'allowSingleLine': true }],
-      'no-use-before-define': 'error',
-      'prefer-const': 'error',
-      'no-unneeded-ternary': 'error',
-      'no-shadow': 'error',
-      'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0, 'maxBOF': 0 }],
-      'import/order': [
-        'error',
-        {
-          'groups': ['type', 'builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-          'pathGroups': [
-            { 'pattern': 'react', 'group': 'external', 'position': 'before' },
-            { 'pattern': '@next/*', 'group': 'internal', 'position': 'before' },
-            { 'pattern': '@/**', 'group': 'internal' },
-            { 'pattern': '*', 'group': 'external', 'position': 'after' }
-          ],
-          'pathGroupsExcludedImportTypes': ['react'],
-          'newlines-between': 'always',
-          'alphabetize': { 'order': 'asc', 'caseInsensitive': true }
-        }
-      ],
     },
-
   }
 ]
 
